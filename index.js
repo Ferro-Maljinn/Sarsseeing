8a9feaa2fe25e0b381719e39b09828534f84f420
Adalia da Silva <ada.future.work@gmail.com>
2022-03-02 14:57:39 +0100

    added: virus animation + Score

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log("Am I working?");
 
 let gameIsOver = false;
+let score = 0;
+let interval = 0;
 
 //Lounch screen; Canvas screen; GameOver screen
 let firstScreen = document.querySelector("#first-screen");
@@ -12,8 +14,8 @@ let startBtn = document.querySelector("#start-btn");
 let restartBtn = document.querySelector("#restart-btn");
 
 //character variables
-let sarsHeight = 90;
-let sarsWidth = 66;
+let sarsHeight = 100;
+let sarsWidth = 90;
 let sarsX = 20;
 let sarsY = 500 - sarsHeight - 20;
 
@@ -30,30 +32,13 @@ let vaxLength = 100;
 let vaxHeight = 100; */
 
 //Array of virus Objects
-let virusArray = [
-  { x: covX, y: covY },
-  /*   { x: covX - 300, y: covY + 100 },
-  { x: covX + 300, y: covY + 800 },
-  { x: covX - 400, y: covY + 100 },
-  { x: covX, y: covY + 100 },
-  { x: covX + 200, y: covY + 1000 },
-  { x: covX + 100, y: covY + 300 },
-  { x: covX + 200, y: covY + 50 },
-  { x: covX + 500, y: covY + 40 },
-  { x: covX + 900, y: covY + 10 },
-  { x: covX + 200, y: covY + 100 }, */
-];
-//Array of vaxxine Objects
-/* let vaxxineArray = [
-  { x: vaxX + 9000, y: vaxY + 400 },
-  { x: vaxX - 8000, y: vaxY + 100 },
-  { x: vaxX + 5000, y: vaxY + 800 },
-]; */
+let virusArray = [{ x: covX, y: covY, i: 0 }];
 
 class virus {
-  constructor(x, y) {
+  constructor(x, y, i) {
     this.x = x;
     this.y = y;
+    this.i = i;
     this.length = 100;
     this.width = 100;
   }
@@ -76,38 +61,60 @@ function preload() {
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("second-screen");
+  textAlign(CENTER);
 }
-
-let interval = 0;
 
 function draw() {
   background(bg);
   interval += 1;
+  // delay (in the number of drawings) between changing virus' frames
+  frq = 20;
 
   //character of Sars
   image(sars, sarsX, sarsY, sarsWidth, sarsHeight);
   //random virus spawning
   if (interval % 100 == 0) {
-    virusArray.push(new virus(width, random(20, windowHeight)));
+    virusArray.push(new virus(width, random(20, windowHeight), interval));
   }
 
   //for loop for the looping of the objectArray
   for (let i = 0; i < virusArray.length; i++) {
-    image(virusBright, virusArray[i].x, virusArray[i].y, covLength, covHeight);
+    if (int(int((interval + virusArray[i].i) / frq)) % 2 == 0) {
+      image(
+        virusBright,
+        virusArray[i].x,
+        virusArray[i].y,
+        covLength,
+        covHeight
+      );
+    } else {
+      image(virusDark, virusArray[i].x, virusArray[i].y, covLength, covHeight);
+    }
+  }
+
+  for (let i = 0; i < virusArray.length; i++) {
     virusArray[i].x -= 4;
 
     //collision with virus
     if (
-      sarsX < virusArray[i].x + covLength &&
-      sarsX + sarsWidth > virusArray[i].x &&
-      sarsY < virusArray[i].y + covLength &&
-      sarsHeight + sarsY > virusArray[i].y
+      sarsX < virusArray[i].x + covLength - 50 && //left
+      sarsX + sarsWidth > virusArray[i].x + 50 && //Right
+      sarsY < virusArray[i].y - 50 + covLength && //Top
+      sarsHeight + sarsY > virusArray[i].y + 50 //Bottom
     ) {
       gameIsOver = true;
     }
     /*    if (virusArray[i].x < -500) {
       virusArray[i].x = 2000;
     } */
+    if (virusArray[i].x < 0) {
+      virusArray.splice(i, 1);
+      score = score + 1;
+      if (gameIsOver == true) {
+        score = 0;
+      }
+    }
+    console.log(score);
   }
 
   // function to move my char left, right, up and down
@@ -123,6 +130,11 @@ function draw() {
   if (gameIsOver) {
     gameOver();
   }
+  // status bar
+  fill(255, 204, 0);
+  textSize(64);
+  text("Score:", 200, 100);
+  text(score, 350, 100);
 }
 
 //Stop draw function to reset objects + display game-over screen
@@ -130,6 +142,9 @@ function gameOver() {
   firstScreen.style.display = "none";
   secondScreen.style.display = "none";
   thirdScreen.style.display = "flex";
+  sarsX = 20;
+  sarsY = 500 - sarsHeight - 20;
+  score = 0; //left
   noLoop();
 }
 
